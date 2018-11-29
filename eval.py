a534d33cff197bd7af0d9f5bc398c411ccddc078
zxc916443179 <zxc916443179@163.com>
2018-11-29 20:39:10 +0800

    remove useless print and add prediction

--- eval.py
+++ eval.py
@@ -18,7 +18,8 @@ with graph.as_default():
         saver.restore(sess, checkpoint_file)
 
         encode = graph.get_operation_by_name("hidden_layer_128/Sigmoid").outputs[0]
-        recon = graph.get_operation_by_name("fn_score/score").outputs[0]
+        recon = graph.get_operation_by_name("finetuning_decoder_3/Sigmoid").outputs[0]
+        print(recon)
         input_x = graph.get_operation_by_name("input_x").outputs[0]
         mask = graph.get_operation_by_name("mask").outputs[0]
         dataset = u.loadDataset(batch_size=flags.max, max=flags.max)
@@ -28,6 +29,7 @@ with graph.as_default():
         encoder_result, recon_result = sess.run([encode, recon], feed_dict={
             input_x: dataset, mask: mask_ts})
         n_examples = 15
+        print(encoder_result.shape)
         fig, axs = plt.subplots(2, n_examples, figsize=(10, 2))
         for example_i in range(n_examples):
             axs[0][example_i].imshow(
@@ -36,11 +38,15 @@ with graph.as_default():
             axs[1][example_i].imshow(
                 # np.reshape([recon[example_i, :] + mean_img], (28, 28)))
                 np.reshape([recon_result[example_i, :]], (15, 15)))
+        plt.show()
+        plt.waitforbuttonpress()
         print ('Plot complete now showing...')
         with open("./svm.model", "rb") as f:
             new_svm = pickle.load(f)
             # print(encoder_result.shape)
+            # prediction = new_svm.predict(encoder_result)
+            # plt.scatter(range(10000) ,prediction[: 10000])
             pre = new_svm.decision_function(encoder_result).ravel() * -1
-            plt.plot(range(1000), pre[:1000])
+            plt.plot(range(10000), pre[:10000])
             plt.show()
-            # plt.waitforbuttonpress()
+            plt.waitforbuttonpress()
