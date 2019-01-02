44df71134998fc342773fcfb5942c3e1a329916a
unknown <zxc916443179@163.com>
2019-01-02 11:01:44 +0800

    cs validation

--- eval.py
+++ eval.py
@@ -11,7 +11,7 @@ import pickle
 tf.flags.DEFINE_string("datasetPath", './data/appearance_spliced_images/appearance.p', "dataset path")
 tf.flags.DEFINE_integer("max", 10000, "max number of dataset")
 tf.flags.DEFINE_string("checkpoint_dir", "none", "loading latest checkpoint")
-
+tf.flags.DEFINE_string('label_dir', './label/label15.p', "dir of label")
 flags = tf.flags.FLAGS
 graph = tf.Graph()
 with graph.as_default():
@@ -28,6 +28,7 @@ with graph.as_default():
         input_x = graph.get_operation_by_name("input_x").outputs[0]
         mask = graph.get_operation_by_name("mask").outputs[0]
         # dataset = u.loadDataset(batch_size=flags.max, max=flags.max)
+        label = utils.loadlabel(dir=flags.label_dir)
         dataset = utils.load_whole_dataset(max = flags.max, dataset_dir=flags.datasetPath)
         # dataset = next(dataset)
         mask_ts = np.random.binomial(1, 1, dataset.shape)
@@ -44,19 +45,34 @@ with graph.as_default():
         #         np.reshape([recon_result[example_i, :]], (15, 15)))
         # plt.show()
         # plt.waitforbuttonpress()
-        # print ('Plot complete now showing...')
+        print ('Plot complete now showing...')
+        accs = []
+        test = utils.load_whole_dataset(max = len(label), dataset_dir="./label.appearance15.p")
+        mask_ts = np.random.binomial(1, 1, test.shape)
+        test_result = sess.run([encode], feed_dict={
+            input_x: test, mask: mask_ts
+        })
+        print('starting cross fold validation')
+        for i in range(0, 0.1, 0.0001):
+            print('step i:{}'.format(i))
+            clf = svm.OneClassSVM(kernel='rbf', gamma='auto', nu=i)
+            clf.fit(encoder_result)
+            pre = clf.predict(test_result)
+            prediction = np.equal(pre, label)
+            accuracy = np.mean(np.cast[np.float](prediction))
+            print('accuracy:{:g}'.format(accuracy))
+            accs.append(accuracy)
+        i = np.argmax(accs)
+        print('biggest nu is:{} acc:{:g}'.format(i * 0.0001, accs[i]))
         # with open("./svm.model", "rb") as f:
         #     new_svm = pickle.load(f)
-        #     # print(encoder_result.shape)
         #     pre = new_svm.predict(encoder_result)
         #     plt.scatter(range(pre.shape[0]) ,pre)
-        #     # pre = new_svm.decision_function(encoder_result).ravel() * -1
-        #     # print(pre[:100])
-        #     print(pre.shape)
-        #     # plt.plot(range(pre.shape[0]), pre)
         #     plt.waitforbuttonpress()
-        clf = svm.OneClassSVM(kernel='rbf', gamma='auto', nu=1e-3)
-        clf.fit(encoder_result)
-        with open('./svm.model', 'wb') as m:
-            pickle.dump(clf, m)
-        
+        #     prediction = np.equal(pre, label)
+        #     accuracy = np.mean(np.cast[np.float](prediction))
+        #     print('accuracy:{:g}'.format(accuracy))
+        # clf = svm.OneClassSVM(kernel='rbf', gamma='auto', nu=1e-3)
+        # clf.fit(encoder_result)
+        # with open('./svm.model', 'wb') as m:
+        #     pickle.dump(clf, m)
